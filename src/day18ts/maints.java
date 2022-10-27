f3cf8bfa93529448a85091aa2e49ce5a44aaefac
葉子維 <zi-wei@yeziweide-MacBook-Pro.local>
2022-10-27 17:27:45 +0800

    Initial commit

---
+++ src/day18ts/maints.java
@@ -0,0 +1,18 @@
+package day18ts;
+
+import java.util.Scanner;
+
+class maints{
+    public static void main(String[] args) {
+        Scanner datain = new Scanner(System.in);
+        String st=datain.next();
+        Integer da = Integer.getInteger(st);
+        for (int i=0;i<da;i++){
+            System.out.println(i+1+":");
+        }
+    }
+}
+
+
+
+
